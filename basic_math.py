59fa3d6c8e4a1dd35c97399fdfd61d67b7cfcd59
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-09-17 18:25:04 +0000

    did a bunch of math in python

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# Copyright(c) 2022 Ioana Marinescu All rights reserved.
+# Created By: Ioana Marinescu
+
+# Date: Sept. 16, 2022
+# doing basic math with python
+import math
+
+
+def main():
+    print(1 + 17.9)
+    print(17.9 - 1)
+    print(2 * 4.3)
+    print(2 / 4.3)
+    print(math.pow(4, 2))
+    print(math.sqrt(16))
+
+
+if __name__ == "__main__":
+    main()
