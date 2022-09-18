ea8eb2c8309d60b248fc4bafebeabc8dc3b8f783
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-09-18 18:32:20 +0000

    are and perimeter calculations are done

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Copyright(c) 2022 Ioana Marinescu All rights reserved.
+# Created By: Ioana Marinescu
+
+# Date: Sept. 16, 2022
+# calculates the area and perimeter of a rectangle with the dimensions of 5cm by 23 cm
+
+
+def main():
+    # calculates the area and perimeter
+    print("If a rectangle has the dimensions: ")
+    print("5cm x 23cm\n")
+    print("Area = ", 5 * 23)
+    print("Perimeter = ", 2 * (5 + 23))
+
+
+if __name__ == "__main__":
+    main()
